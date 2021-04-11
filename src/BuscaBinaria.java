a0d47127da9c504265a8d5e48debd71bef4244b1
David <williancelani@hotmail.com>
2021-04-11 15:12:50 -0300

    100% ok

--- src/BuscaBinaria.java
+++ src/BuscaBinaria.java
@@ -9,12 +9,12 @@ public class BuscaBinaria {
         int meio;
         int inicio, fim;
 
-        inicio = turma.get(0).getRgm();
-        fim = turma.get(turma.size() - 1).getRgm();
+        inicio = 0;
+        fim = turma.size() - 1;
 
         while (inicio <= fim)
         {
-            meio = (inicio + fim) / 2;
+            meio = ((int) (inicio + fim) / 2);
             if(turma.get(meio).getRgm() == rgm) return meio;
             else if(turma.get(meio).getRgm() < rgm) inicio = meio + 1;
             else if(turma.get(meio).getRgm() > rgm) inicio = meio - 1;
